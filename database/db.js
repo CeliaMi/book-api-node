81d09dc8c1eab7f617311832b55880c5ddbc354d
Celia <celiamillanalemany@gmail.com>
2023-12-17 11:02:04 +0100

    data base conection

---
+++ database/db.js
@@ -0,0 +1,12 @@
+import { Sequelize } from "sequelize";
+import { DB_NAME,DB_PASSWORD,DB_USER } from "../config";
+
+const db = new Sequelize(DB_NAME, DB_USER,DB_PASSWORD, {
+		host: 'localhost',
+		dialect: 'mysql',
+		define: {
+			timestamps: false 
+		}
+}) 
+
+export default db;
